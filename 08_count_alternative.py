4f2bf7801269da79404ec782bec5047a33285565
Andre <andrepetilunalolong@gmail.com>
2025-03-31 23:13:14 +0800

    created a function

--- 08_count_alternative.py
+++ 08_count_alternative.py
@@ -2,4 +2,10 @@
 # check the given statement
 # check if the target value is in the statement
 # if yes then add one to the count and repeat untill how many is detected
-# display 
+# display
+def count_alternative(text, target):
+    count = 0
+    for i in range(len(text) - len(target) + 1):
+        if text[i:i + len(target)] == target:
+            count += 1
+    return count
