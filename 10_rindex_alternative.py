c107d10cb772665c5f1411348089ab86d6ceea22
Andre <andrepetilunalolong@gmail.com>
2025-03-31 23:49:18 +0800

    rindex() alternative

--- 10_rindex_alternative.py
+++ 10_rindex_alternative.py
@@ -3,3 +3,12 @@
 # check the given input
 # if the input was there then count the coordinate backwards
 # if the input was not there then return -1 
+def rindex_alternative(text, target):
+    for i in range(len(text)- 1, -1, -1):
+        if text[i:i + len(target)] == target:
+            return i
+    return -1
+
+statement = input("Enter something: ")
+target_input = input("Enter the target input you want to use rindex on: ")
+print(rindex_alternative(statement, target_input))
