49c0d68cc0c5391ee2e8a86a8cd9348f5864b56b
Andre <andrepetilunalolong@gmail.com>
2025-03-31 22:44:01 +0800

    created a function

--- 05_startswith_alternative.py
+++ 05_startswith_alternative.py
@@ -1,3 +1,7 @@
 # create a function
 # check if it starts with the given prefix
 # return true if yes and false if not
+def startswith_alternative(text, prefix):
+    if len(prefix) > len(text):
+        return False
+    return text[:len(prefix)] == prefix
