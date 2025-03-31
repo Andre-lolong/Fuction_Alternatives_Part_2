af77690b3b2cfd9eb0a4f4fda7079ce61bdaed83
Andre <andrepetilunalolong@gmail.com>
2025-03-31 21:46:10 +0800

    rstrip() alternative

--- 01_rstrip_alternative.py
+++ 01_rstrip_alternative.py
@@ -2,3 +2,11 @@
 # check if the end is a space
 # remove the space
 # display output
+def rstrip_alternative(text):
+    i = len(text)
+    while i > 0 and text[i - 1] == " ":
+        i -= 1
+    return text[:i]
+    
+statement = input("Enter a text with spaces at the end: ")
+print(rstrip_alternative(statement))
