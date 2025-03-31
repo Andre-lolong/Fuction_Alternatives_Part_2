f67c17a86b07f065090d817e458da82d6f277738
Andre <andrepetilunalolong@gmail.com>
2025-03-31 22:07:11 +0800

    created a function

--- 04_islower_alternative.py
+++ 04_islower_alternative.py
@@ -1,3 +1,9 @@
 # create a function
 # identify the letters
 # if uppercase then return false and true if lowercase
+def islower_alternative(text):
+    for letters in text:
+        if "A" <= letters <= "Z":
+            return False
+    return True
+
