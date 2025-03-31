2c3d5043f829ee8655ec8c1c63365ec7777a875d
Andre <andrepetilunalolong@gmail.com>
2025-03-31 23:31:57 +0800

    pseudocode

---
+++ 10_rindex_alternative.py
@@ -0,0 +1,5 @@
+# same as index but backwards
+# create a function
+# check the given input
+# if the input was there then count the coordinate backwards
+# if the input was not there then return -1 
