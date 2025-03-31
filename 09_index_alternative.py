4fd2af69d2691fcff8637f714173b7cb4127237a
Andre <andrepetilunalolong@gmail.com>
2025-03-31 23:22:44 +0800

    created a function

--- 09_index_alternative.py
+++ 09_index_alternative.py
@@ -2,3 +2,8 @@
 # check the given input
 # if the target input  was there then count the coordinatee
 # if the target was not there then return -1
+def index_alternative(text, target):
+    for i in range(len(text) - len(target) + 1):
+        if text[i:i + len(target)] == target:
+            return i
+    return -1
