efe69593d6345c863d5d6a313a17f956ecb3d570
Andre <andrepetilunalolong@gmail.com>
2025-03-31 23:09:00 +0800

    pseudocode

---
+++ 08_count_alternative.py
@@ -0,0 +1,5 @@
+# create a function
+# check the given statement
+# check if the target value is in the statement
+# if yes then add one to the count and repeat untill how many is detected
+# display 
