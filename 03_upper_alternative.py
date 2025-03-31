d406ffdca8a574ecbb1c530563b2c72ad3cc8dd5
Andre <andrepetilunalolong@gmail.com>
2025-03-31 22:01:09 +0800

    upper() alternative

--- 03_upper_alternative.py
+++ 03_upper_alternative.py
@@ -2,3 +2,16 @@
 # check for lowercase letters
 # get the ASCII number and shift lowercase to uppercase
 # add all the uppercase to the result
+def upper_alternative(text):
+  result = ""
+  for letter in text:
+    if 'a' <= letter <= 'z':
+      uppercase_letter = chr(ord(letter) - 32)
+      result += uppercase_letter
+    else:
+      result += letter
+  return result
+
+text = input("Enter something with lowercase: ")
+uppercase = upper_alternative(text)
+print(uppercase)
