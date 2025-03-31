daa0ce6b95485f6405476433a5fd8d809e4c0d9c
Andre <andrepetilunalolong@gmail.com>
2025-03-31 22:46:31 +0800

    startswith() alternative

--- 05_startswith_alternative.py
+++ 05_startswith_alternative.py
@@ -5,3 +5,8 @@ def startswith_alternative(text, prefix):
     if len(prefix) > len(text):
         return False
     return text[:len(prefix)] == prefix
+
+main_text = input("Enter something: ")
+its_prefix = input("Enter a prefix: ")
+
+print(startswith_alternative(main_text, its_prefix))
