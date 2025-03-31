ad9fcaead7a049dadb9e464ef75ed41633b3f3dd
Andre <andrepetilunalolong@gmail.com>
2025-03-31 22:53:46 +0800

    rjust() alternative

--- 06_rjust_alternative.py
+++ 06_rjust_alternative.py
@@ -9,3 +9,7 @@ def rjust_alternative(text, width):
     
     space_to_add = width - text_len
     return " " * space_to_add + text
+
+text = input("Enter a text: ")
+spaces = int(input("Enter how many spaces you want: "))
+print(rjust_alternative(text, spaces))
