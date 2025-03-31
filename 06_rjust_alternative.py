28afbffb395d5112021aba271c4d27477146163f
Andre <andrepetilunalolong@gmail.com>
2025-03-31 22:50:56 +0800

    created a function

--- 06_rjust_alternative.py
+++ 06_rjust_alternative.py
@@ -2,3 +2,10 @@
 # choose width to be justified
 # add it to the text
 # display
+def rjust_alternative(text, width):
+    text_len = len(text)
+    if text_len >= width:
+        return text
+    
+    space_to_add = width - text_len
+    return " " * space_to_add + text
