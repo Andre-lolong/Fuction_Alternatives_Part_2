5c5f37ffc899acb929f23cf5c099a8b83c143fcd
Andre <andrepetilunalolong@gmail.com>
2025-03-31 23:00:16 +0800

    created a function

--- 07_zfill_alternative.py
+++ 07_zfill_alternative.py
@@ -2,3 +2,7 @@
 # specify the width
 # fill the beginning with zeros to satisfy the width
 # display
+def zfill_alternative(text, width):
+    if len(text) >= width:
+        return text
+    return "0" * (width - len(text)) + text
